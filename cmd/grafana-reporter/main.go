ae3c58ca70af15fcba1722081562f94d8e2cd933
jiqixiaomao <215232426@qq.com>
2022-10-13 11:43:41 +0800

    Update main.go

--- cmd/grafana-reporter/main.go
+++ cmd/grafana-reporter/main.go
@@ -22,8 +22,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/jiqixiaomao/reporter/grafana"
+	"github.com/jiqixiaomao/reporter/report"
 	"github.com/gorilla/mux"
 )
 
